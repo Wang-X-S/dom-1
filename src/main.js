4e5d516eeb9af7f8f2cd0bbcc535d659c7880c93
13jun <820373584@qq.com>
2021-06-26 13:14:59 +0800

    实现 after before append wrap

--- src/main.js
+++ src/main.js
@@ -1,4 +1,14 @@
-const div = dom.create('               <td>123</td>')
-
-
+const div = dom.create('<td>123</td>')
 console.log(div)
+
+
+dom.after(test,div)
+
+const div2 = dom.create('<span>haha<span>')
+dom.before(test2,div2)
+
+const div3 = dom.create('<div>儿子</div>')
+dom.append(test2,div3)
+
+const div4 = dom.create('<div>新建父节点</div>')
+dom.wrap(wrap,div4)
